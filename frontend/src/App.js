eb7568028bf68cd055ad5eee4f85eb4cb0b19383
Steven Woodward <steveanthony999@gmail.com>
2022-03-22 22:56:24 -0700

    added header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import FeedbackDetailPage from './Pages/FeedbackDetailPage';
 import NewFeedbackPage from './Pages/NewFeedbackPage';
 import RoadmapPage from './Pages/RoadmapPage';
 
+import './styles/styles.css';
+
 function App() {
   return (
     <Router>
--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,4 +1,25 @@
+import BackgroundHeaderImg from '../assets/suggestions/desktop/background-header.png';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className='HomePage'>
+      <div className='container'>
+        <div className='left'>
+          <div
+            className='top'
+            style={{
+              background: `url(${BackgroundHeaderImg}) no-repeat center center/cover`,
+            }}>
+            <h2 className='h2 text-white'>Frontend Mentor</h2>
+            <br />
+            <p className='body-2 text-white'>Feedback Board</p>
+          </div>
+          <div className='middle'>middle</div>
+          <div className='bottom'>bottom</div>
+        </div>
+        <div className='right'>right</div>
+      </div>
+    </div>
+  );
 };
 export default HomePage;
