f9218c3a40b7c989824013e7e4c7f4d13035f779
Steven Woodward <steveanthony999@gmail.com>
2022-03-23 20:55:25 -0700

    assigned feedback data

--- frontend/src/Components/ProductFeedbackComponent.jsx
+++ frontend/src/Components/ProductFeedbackComponent.jsx
@@ -2,16 +2,17 @@ const ProductFeedbackComponent = ({ feedback }) => {
   return (
     <div className='ProductFeedbackComponent'>
       <div className='container'>
-        <div className='left'></div>
+        <div className='left'>
+          <p>{feedback.upvotes}</p>
+        </div>
         <div className='middle'>
           <h3 className='h3'>{feedback.title}</h3>
-          <p className='body-1'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
-            obcaecati?
-          </p>
-          <div className='btn text-blue body-3'>Button Name</div>
+          <p className='body-1'>{feedback.description}</p>
+          <div className='btn text-blue body-3'>{feedback.category}</div>
         </div>
-        <div className='right'></div>
+        <div className='right'>
+          <p>{feedback.comments ? feedback.comments.length : 0}</p>
+        </div>
       </div>
     </div>
   );
